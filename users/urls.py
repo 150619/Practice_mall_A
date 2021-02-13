6a335f040f31f692548d2f04f7e56c4bb372f282
sixseven67 <rtyhxrtyhrtyh78782@gmail.com>
2021-02-13 23:38:32 +0800

    访问类视图

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# 访问类视图views.类名.as_view()
 urlpatterns = [
-    path('users/register', views.register)
+    path('users/register', views.Register.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# def register(request):
+#     if request.method == 'GET':
+#         return HttpResponse('注册页面')
+#     if request.method == 'POST':
+#         return HttpResponse('提交注册信息')
+from django.views import View
 
-def register(request):
-    if request.method == 'GET':
+
+class Register(View):
+    def get(self, request):
         return HttpResponse('注册页面')
-    if request.method == 'POST':
-        return HttpResponse('提交注册信息')
+
+    def post(self, request):
+        return HttpResponse('注册逻辑')
