d498cf4e4a26cb892c043f3a484207b7429c799b
sixseven67 <rtyhxrtyhrtyh78782@gmail.com>
2021-02-14 23:30:07 +0800

    在类视图中添加装饰器,@method_decorator定义在类外面name=dispatch是全部添加

--- users/views.py
+++ users/views.py
@@ -6,10 +6,22 @@ from django.shortcuts import render
 #         return HttpResponse('注册页面')
 #     if request.method == 'POST':
 #         return HttpResponse('提交注册信息')
+from django.utils.decorators import method_decorator
 from django.views import View
 
 
+def my_decorator(func):
+    def inner_func(request, *args, **kwargs):
+        print(f'请求路径{request.path}')
+        result = func(request, *args, **kwargs)
+        return result
+
+    return inner_func
+
+
+@method_decorator(my_decorator, name='post')
 class Register(View):
+    @method_decorator(my_decorator)
     def get(self, request):
         return HttpResponse('注册页面')
 
