51a79c6ecff54e5c042a407ec101cbb00417992a
sixseven67 <rtyhxrtyhrtyh78782@gmail.com>
2021-02-20 14:01:39 +0800

    注册2.0

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from django.http import JsonResponse
 from django.views import View
@@ -9,56 +10,64 @@ from apps.users.models import User
 class RegisterName(View):
     # 解析路径参数
     def get(self, request, username):
-        # 与数据库中数据做对比
+        # 从数据库中查询数据
         count = User.objects.filter(username=username).count()
-        if count > 0:
-            return JsonResponse({'code': '400', 'errmsg': '用户名已存在', 'count': count})
-        else:
-            return JsonResponse({'code': '0', 'errmsg': 'ok', 'count': count})
+        return JsonResponse({'code': '0', 'errmsg': 'ok', 'count': count})
 
 
 class RegisterMobile(View):
     def get(self, request, mobile):
+        # 从数据库中查询数据
         count = User.objects.filter(mobile=mobile).count()
-        if count > 0:
-            return JsonResponse({'code': '400', 'errmsg': '手机号已存在', 'count': count})
-        else:
-            return JsonResponse({'code': '0', 'errmsg': 'ok', 'count': count})
+        return JsonResponse({'code': '0', 'errmsg': 'ok', 'count': count})
 
 
 class Register(View):
     def post(self, request):
         # 解析请求体参数
+        # json_bytes = request.body
+        # json_str = json_bytes.decode()
         json_dict = json.loads(request.body)
         # 获取username
         username = json_dict.get('username')
+        # 获取password和password2
+        password = json_dict.get('password')
+        password2 = json_dict.get('password2')
+        # 获取mobile
+        mobile = json_dict.get('mobile')
+        # 获取allow
+        allow = json_dict.get('allow')
+        # 判断必传参数是否全部存在
+        if not all([username, password, password2, mobile, allow]):
+            return JsonResponse({'code': '400', 'errmsg': '缺少必传参数'})
+        # 判断username的格式
+        if not re.match(r'[a-zA-Z0-9_-]{5,20}', username):
+            return JsonResponse({'code': '400', 'errmsg': '用户名格式错误'})
         # 判断数据库中是否已经存在一个username
         count = User.objects.filter(username=username).count()
         # 存在返回错误码
         if count > 0:
             return JsonResponse({'code': '400', 'errmsg': '用户名已存在'})
-        # 获取password和password2
-        password = json_dict.get('password')
-        password2 = json_dict.get('password2')
+        # 判断密码格式
+        if not re.match(r'^[a-zA-Z0-9]{8,20}$', password):
+            return JsonResponse({'code': '400', 'errmsg': '密码格式错误'})
         # 判断password和password2是否相等,不相等返回错误码
         if password != password2:
             return JsonResponse({'code': '400', 'errmsg': '两次输入的密码不一致'})
-        # 获取mobile
-        mobile = json_dict.get('mobile')
+        # 判断手机号格式
+        if not re.match(r'1[3-9]\d{9}', mobile):
+            return JsonResponse({'code': '400', 'errmsg': '手机号格式不正确'})
         # 判断手机号是否存在
         count = User.objects.filter(mobile=mobile).count()
         if count > 0:
             return JsonResponse({'code': '400', 'errmsg': '手机号已存在'})
-        # 判断手机号格式
 
         # 获取sms_code
         # sms_code = json_dict.get('sms_code')
         # 与redis中的真实验证码做对比
         # 如果不相等返回错误码
 
-        # 获取allow
-        allow = json_dict.get('allow')
-        # 不是True返回错误码
+        # 不等于True返回错误码
         if allow != 'True':
             return JsonResponse({'code': '400', 'errmsg': '请同意用户协议'})
         # 将获取到的数据保存到数据库
